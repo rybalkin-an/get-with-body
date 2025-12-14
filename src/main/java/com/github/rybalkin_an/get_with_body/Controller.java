a4f49bb87c418376ec88c365ce7d65b47e2c7a00
Aleksei <rybalkine@gmail.com>
2025-12-14 13:01:00 +0100

    added swagger ui

--- src/main/java/com/github/rybalkin_an/get_with_body/Controller.java
+++ src/main/java/com/github/rybalkin_an/get_with_body/Controller.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Examples")
 public class Controller {
 
     private final Service service;
@@ -18,11 +21,13 @@ public class Controller {
         this.service = service;
     }
 
+    @Operation(summary = "GET example (with request body)")
     @GetMapping(value = "/getExample", produces = "text/plain")
     public String getExample(@RequestBody String requestBody) {
         return service.getExample(requestBody);
     }
 
+    @Operation(summary = "POST example")
     @PostMapping(value = "/postExample", produces = "text/plain")
     public String postExample() {
         return service.postExample();
